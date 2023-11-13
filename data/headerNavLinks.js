567a83c94da0a548c21707d64f082b155b744187
shaneboulden <shane.boulden@gmail.com>
2023-11-13 12:23:00 +1000

    Add etherpad links and fix broken roles

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,5 +1,6 @@
 const headerNavLinks = [
-  { href: '/workshop', title: 'Exercises' }
+  { href: '/workshop', title: 'Exercises' },
+  { href: 'https://etherpad.wikimedia.org/p/tssc-workshop-nov-23', title: 'Etherpad'}
 ]
 
 export default headerNavLinks
